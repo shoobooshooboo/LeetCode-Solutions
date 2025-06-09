6a1b1d93199be9a5c5e8deed5cb515d179551c34
Noel vanSchaick <johnonymous4@gmail.com>
2025-06-09 13:00:41 -0700

    longest substring v2 O(n)

--- rust/longest-substring.rs
+++ rust/longest-substring.rs
@@ -1,27 +1,31 @@
 use std::collections::HashSet;
+use std::cmp::max;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         let input_len = s.len();
+        //left and right boundary
+        let (mut i, mut j) = (0, 0);
         let mut max_length = 0;
-        for i in 0..input_len{
-            //early break of current max_length if there can't be a longer substring
-            if max_length + i > input_len{
-                break;
-            }
-
-            let mut length = 0; 
-            //hashset to keep track of unique characters
-            let mut chars = HashSet::new();
-            for c in s[i..].chars(){
-                //count unique characters and break on a duplicate
-                if !chars.insert(c){
-                    break;
+        //for tracking unique chars
+        let mut char_set = HashSet::new();
+        let chars: Vec<char> = s.chars().collect();
+        while j < input_len{
+            //check if new character is a duplicate
+            if char_set.insert(&chars[j]){
+                //if it is, move right the boundary
+                j += 1;
+            }else{
+                //otherwise, remove the first character
+                char_set.remove(&chars[i]);
+                //and adjust both bounds as needed
+                if i == j{
+                    j += 1;
                 }
-                length += 1;
+                i += 1;
             }
-            //set the new max_length
-            max_length = std::cmp::max(max_length, length);
+            //update the max length
+            max_length = max(max_length, j - i);
         }
 
         max_length as i32
