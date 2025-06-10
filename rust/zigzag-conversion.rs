e3b1e30d6d69c4f0e9b41bf0934e5b66e22684ef
Noel vanSchaick <johnonymous4@gmail.com>
2025-06-10 15:56:10 -0700

    Zigzag Conversion v2

--- rust/zigzag-conversion.rs
+++ rust/zigzag-conversion.rs
@@ -1,32 +1,17 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        //map each row to the string in the row
-        let mut row_map = HashMap::new();
-        for i in 0..num_rows{
-            row_map.insert(i, String::new());
-        }
+        //elegent solution with iterators
+        let mut result: Vec<(i32, char)>= (0..num_rows) // 0,1,2,3
+            .chain((1..num_rows - 1).rev()) //0,1,2,3,2,1
+            .cycle() //0,1,2,3,2,1,0,1...
+            .zip(s.chars()) //effectively enumerating the input 
+            .collect();
 
-        //walk through the string and collect the chars
-        let mut row = 0;
-        let mut inc = false;
-        for c in s.chars(){
-            println!("{row}");
-            row_map.get_mut(&row).unwrap().push(c);
-            if row == 0 || row == num_rows - 1{
-                inc = !inc;
-            }
-            if num_rows != 1{
-                row += if inc {1} else {-1};
-            }
-        }
-
-        //place all the rows into a result string
-        let mut result = String::new();
-        for i in 0..num_rows{
-            result.push_str(row_map.get(&i).unwrap());
-        }
-
-        result
+        //sort by row
+        result.sort_by_key(|&(row, _)| row);
+        
+        //remove rows
+        result.into_iter().map(|(_, c)| c).collect()
     }
 }
