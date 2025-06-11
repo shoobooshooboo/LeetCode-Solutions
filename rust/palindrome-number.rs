e4e7777fbfe00f5a18689a8e4b9e74a71310c895
Noel vanSchaick <johnonymous4@gmail.com>
2025-06-10 21:24:03 -0700

    Palindrome Number v1

--- rust/palindrome-number.rs
+++ rust/palindrome-number.rs
@@ -1,9 +1,12 @@
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
-        //very obvious, easy solution.
+        //another very obvious answer, but slightly faster... for some reason.
         let x = x.to_string();
-        let rev: String = x.chars().rev().collect();
-
-        x == rev
+        for (a, b) in x.chars().zip(x.chars().rev()){
+            if a != b{
+                return false;
+            }
+        }
+        true
     }
 }
