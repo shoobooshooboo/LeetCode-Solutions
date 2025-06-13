bdd9d3a95cf948f321b4fa837fddfe16bf529c7a
Noel vanSchaick <johnonymous4@gmail.com>
2025-06-13 11:13:47 -0700

    3Sum v2

--- rust/three-sum.rs
+++ rust/three-sum.rs
@@ -1,22 +1,48 @@
-use std::collections::HashSet;
-use std::collections::HashMap;
 impl Solution {
-    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        //solution very similar to 2sum using a map of complements
-        let mut complements: HashMap<i32,usize> = nums.iter().enumerate().map(|(i, n)| (-n, i)).collect();
-        let mut result = HashSet::new();
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        //less intuitive two pointer solution
 
-        for i in 0..nums.len(){
-            for j in (i + 1)..nums.len(){
-                if let Some((n, k)) = complements.get_key_value(&(nums[i] + nums[j])){
-                    if *k != i && *k != j{
-                        let mut triplet = vec![-n, nums[i], nums[j]];
-                        triplet.sort();
-                        result.insert(triplet);
+        //sorting helps avoid duplicates and junk
+        nums.sort();
+        let mut result = Vec::new();
+
+        for i in 0..(nums.len() - 2){
+            //skip duplicates
+            if nums.get(i) == nums.get(i - 1){
+                continue;
+            }
+
+            //these are the afformentioned two pointers
+            let (mut left, mut right) = (i + 1, nums.len() - 1);
+            while left < right{
+                //get the sum
+                let sum = nums[i] 
+                    + nums[left] 
+                    + nums[right];
+                if sum == 0{
+                    //update the result and pointers
+                    result.push(vec!(nums[i], nums[left], nums[right]));
+                    left += 1;
+                    right -= 1;
+
+                    while left < right && nums[left] == nums[left - 1]{
+                        left += 1;
                     }
+                    while right > left && nums[right] == nums[right + 1]{
+                        right -= 1;
+                    }
+                }
+                else if sum > 0{
+                    //too high, so dial it back a little
+                    right -= 1;
+                }
+                else{
+                    //too low, so turn things up a little
+                    left += 1;
                 }
             }
         }
-        result.into_iter().collect()
+
+        result
     }
 }
