2c0341e0dc4da91831a82542d60145fe34a34039
Noel vanSchaick <johnonymous4@gmail.com>
2025-07-13 13:59:19 -0700

    Is Subsequence (two pointers instead of two iterators)

--- rust/is_subsequence.rs
+++ rust/is_subsequence.rs
@@ -1,19 +1,21 @@
 impl Solution {
     pub fn is_subsequence(s: String, t: String) -> bool {
-        //two iterator solution. basically two pointer but maybe a little dirtier.
-        //really just did this cause i wanted to use a for loop.
-        let mut subsequence = s.chars();
-        let mut sub_c = subsequence.next();
-        for c in t.chars(){
-            match &sub_c{
-                Some(sc) => {
-                    if *sc == c{
-                        sub_c = subsequence.next();
-                    }
-                },
-                None => break,
+        // two pointer approach. looks cleaner but is less versatile than
+        // the iterator approach i used previously.
+        let s = s.as_bytes();
+        let t = t.as_bytes();
+        let (mut i, mut j) = (0, 0);
+        while j < t.len(){
+            if s[i] == t[j]{
+                i += 1;
             }
+            if i > s.len(){
+                break;
+            }
+
+            j += 1;
         }
-        sub_c.is_none()
+
+        i == s.len()
     }
 }
