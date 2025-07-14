ad803232f47e38f9222aacd11f78eef30bd4cc36
Noel vanSchaick <johnonymous4@gmail.com>
2025-07-14 14:12:40 -0700

    Max Number of K-Sum Pairs v2

--- rust/max-number-of-ksum-pairs.rs
+++ rust/max-number-of-ksum-pairs.rs
@@ -1,35 +1,22 @@
-use std::collections::HashMap;
 impl Solution {
-    pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
-        //literally just two-sum but repeatedly in the same loop.
-        let mut counts = HashMap::<i32, u32>::new();
+    pub fn max_operations(mut nums: Vec<i32>, k: i32) -> i32 {
+        //two pointer solution.
+        //first, sort the array
+        nums.sort();
+        //get your two pointers
+        let (mut i, mut j) = (0, nums.len() - 1);
         let mut operations = 0;
-        for n in nums{
-            if n >= k{
-                continue;
-            }
-            let complement = k - n;
-
-            if let Some(c) = counts.get_mut(&n){
-                *c += 1;
-            }
-            else{
-                counts.insert(n, 1);
-            }
-
-            if let Some(c) = counts.get_mut(&complement){
-                if n == complement{
-                    if *c > 1{
-                        *c -= 2;
-                        operations += 1;
-                    }
-                }else{
-                    if *c > 0{
-                        *c -= 1;
-                        *counts.get_mut(&n).unwrap() -= 1;
-                        operations += 1;
-                    }
-                }
+        while i < j{
+            //check the sum. update i, j, and operations as necessary
+            let sum = nums[i] + nums[j];
+            if sum == k{
+                i += 1;
+                j -= 1;
+                operations += 1;
+            } else if sum > k{
+                j -= 1;
+            } else{
+                i += 1;
             }
         }
 
