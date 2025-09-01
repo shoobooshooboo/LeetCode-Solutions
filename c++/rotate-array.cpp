38bb23ed5c9ae153f0dd47f1763055a7e6a4346e
Noel vanSchaick <johnonymous4@gmail.com>
2025-09-01 15:48:58 -0700

    Rotate Array

---
+++ c++/rotate-array.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        std::reverse(nums.begin(), nums.end() - k);
+        std::reverse(nums.end() - k, nums.end());
+        std::reverse(nums.begin(), nums.end());
+    }
+};
